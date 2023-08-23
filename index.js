427acd8242328e3361410f1bd34f45c38b3eb338
sendkite <sendkite1@gmail.com>
2023-08-24 01:20:01 +0900

    feat sendkite-calculator test

---
+++ index.js
@@ -0,0 +1,6 @@
+const math = require('sendkite-calculator');
+
+console.log(math.add(1, 2)); // 3
+console.log(math.subtract(1, 2)); // -1
+console.log(math.multiply(1, 2)); // 2
+console.log(math.divide(1, 2)); // 0.5
